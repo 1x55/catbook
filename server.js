a07616a3cc2ce0095b9c64b50febfb6d75dd1dd7
1x55 <k4i@pm.me>
2023-08-08 14:44:57 +0900

    updated connection to fb

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-require('dotenv').config() //global
-const express = require('express')
-const app = express()
-//Add middleware: (help process requests that go in and out of our server)
-const mongoose = require('mongoose') 
+require('dotenv').config();
+const express = require('express');
+//const path = require('path');
+const app = express();
 const connectDB = require('./config/connectDB')
 const catRoutes = require('./routes/catRoutes');
-const session = require('express-session')
-const passport = require('passport')
+const userRoutes = require('./routes/userRoutes');
+const mongoose = require('mongoose')
+const session = require('express-session');
+const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/userModel')
+const User = require('./models/userModel'); // use the path to your User model
 
 const PORT = process.env.PORT ||  3500
 
@@ -18,10 +19,11 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public')) // middleware configuration in a express.js app that serves static files from the ‘public’ directory and serve it back to client if found
+// middleware configuration in a express.js app that serves static files from the ‘public’ directory and serve it back to client if found
+app.use(express.static('public'));
 
 // Set up view engine (EJS: embedded JavaScript), simplifies the process of generating dynamic HTML content in web applications. By embedding JavaScript code directly into templates, developers can create data-driven and interactive web pages efficiently.
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 //Applications must initialize session support in order to make use of login sessions. In an Express app, session support is added by using express-session middleware.
 
@@ -32,28 +34,29 @@ app.use(session({
   }));
 
 //add the Passport initialization middleware to your express.js app
-app.use(passport.initialize())
-//this middleware will handle user session management. allowing Passprt to serilaze and deserialize user instances into and from the session
-app.use(passport.session())
+app.use(passport.initialize());
+//this middleware will handle user session management. allowing Passport to serilaze and deserialize user instances into and from the session
+app.use(passport.session());
 
 
 passport.use(new LocalStrategy(User.authenticate()));
 //scramble p/w
-passport.serializeUser(User.serializeUser())
+passport.serializeUser(User.serializeUser());
 //descrable p/w
-passport.deserializeUser(User.deserializeUser)
+passport.deserializeUser(User.deserializeUser());
 
 //passing current user info to all routes
-app.use((req,res,next) => {
-    res.locals.currentUser = req.user
-    next()
-})
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+    });
 
 //middleware our server is setup to listen to someone visiting the home page. indicates that the middleware function(s) specified in the 'catRoutes' will be executed for any request made to the root path of the app
-app.use('/', catRoutes)
+app.use('/', userRoutes);
+app.use('/', catRoutes);
 
 //Listen for the 'open' event on the database connection
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
-})
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  })
